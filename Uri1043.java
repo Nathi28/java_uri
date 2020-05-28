5d778c539c1b92e553494942cee3b669357bde5d
nathi28 <004350724@itaud.des.ihf>
2020-05-28 17:20:36 -0300

    Primeiro Commit

---
+++ Uri1043.java
@@ -0,0 +1,33 @@
+import java.util.Scanner;
+public class Main{
+    public static void main(String args[]){
+        Scanner teclado = new Scanner(System.in);
+        float A, B, C;
+        float area;
+        float perimetro;
+
+ 
+
+        A = teclado.nextFloat();
+        B = teclado.nextFloat();
+        C = teclado.nextFloat();
+
+ 
+
+        if (A + B > C && A + C > B && B + C > A){
+            perimetro = A + B + C;
+            System.out.printf("Perimetro = " +perimetro);
+        }
+            else{
+                area = (A+B)*C/2;
+                System.out.printf("Area = " +area);
+            }
+
+ 
+
+
+    }
+
+ 
+
+}
---
+++ Uri1049.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+public class Uri1050{
+    public static void main(String args[]){
+        Scanner teclado = new Scanner(System.in);
+ 
+        int DDD;
+ 
+        DDD = teclado.nextInt();
+ 
+        if (DDD == 61){
+            System.out.println("Brasilia");
+        }
+        else if(DDD == 71){
